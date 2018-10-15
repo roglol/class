b4e8ef7e78eb77c2ea5244352e151ef3e506819e
otiko <otikopotskhoraia@gmail.com>
2018-10-15 17:57:49 +0400

    explicit

---
+++ server/naxe.test.js
@@ -0,0 +1,6 @@
+const utils = require('./naxe')
+
+it('should add two numbers', () =>{
+  let resu = utils.add(33,11)
+  throw new Error('Value not correct')
+})
--- weatherApp/app.js
+++
@@ -1,8 +0,0 @@
-const request = require('request')
-
-request({
-    url:'',
-    json:true
-}, (error,response,body) =>{
-    console.log(body)
-})
